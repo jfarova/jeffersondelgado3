68541e0cb9cfd84a8139e0d1bcc7cd05d4fd8f8d
chenaoh <jfarova112333@gmail.com>
2022-11-20 19:56:36 -0500

    Escribir un programa que lea números enteros indefinidamente hasta que llegue el número 0

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       int num=1;
+       while(num!=0){
+        Console.WriteLine("ingrese numero");
+        num=int.Parse(Console.ReadLine());
+       }
+       Console.WriteLine("finalizado");
+    }
+}
